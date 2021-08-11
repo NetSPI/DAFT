508797407d114d7a38c1b1c25e47b19fa0a6aceb
Nick Landers <nlanders@netspi.com>
2021-08-11 15:29:54 -0600

    Add RTT Support

--- DAFT/Program.cs
+++ DAFT/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
+using System.Runtime.InteropServices;
+using System.IO;
 
 using Mono.Options;
 using DAFT.Modules;
@@ -12,6 +14,50 @@ namespace DAFT
 {
     class Program
     {
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW([MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public static string[] CommandLineToArgs(string commandLine)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if (argv == IntPtr.Zero)
+                throw new System.ComponentModel.Win32Exception();
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+
+        public static string Execute(string commandLine)
+        {
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+            Console.SetError(sw);
+
+            try
+            {
+                Main(CommandLineToArgs(commandLine));
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+            
+            return sw.ToString();
+        }
+
         static void Main(string[] args)
         {
             try
